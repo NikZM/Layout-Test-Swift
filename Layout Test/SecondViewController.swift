40069470b7334c101aa901a11058e350cf0facaf
Nik Medgyesy <nikjzm@gmail.com>
2018-12-18 13:48:08 +0000

    Initial Commit

---
+++ Layout Test/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Layout Test
+//
+//  Created by Nik Medgyesy on 18/12/2018.
+//  Copyright © 2018 Nik Medgyesy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
